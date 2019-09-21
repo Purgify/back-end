801bf521c3422b8391a72379085735a548cfafcc
Kelsey Huse <kelseyhuse30@gmail.com>
2019-09-21 11:58:38 -0500

    successfully get artist data from spotify

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,34 @@
-const BASE_URL = "https://api.spotify.com/"
-
 const functions = require("firebase-functions");
+const SpotifyWebApi = require("spotify-web-api-node");
 const SPOTIFY_CLIENT_ID = functions.config().spotify.client_id;
 const SPOTIFY_CLIENT_SECRET = functions.config().spotify.client_secret;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+const spotifyApi = new SpotifyWebApi({
+  clientId: SPOTIFY_CLIENT_ID,
+  clientSecret: SPOTIFY_CLIENT_SECRET
 });
 
 exports.detectArtist = functions.https.onRequest((request, response) => {
   // get query param from request for the artist name to search
   const artistName = request.query.name;
   // call spotify API to get artist URI
-fetch('${BASE_URL}v1/search?q=name:${artistName}&type=artist')
-.then(artistQ => artistQ.json())
-.then(artistData => response.send(artistData));
+  spotifyApi.clientCredentialsGrant().then(
+    function(data) {
+      console.log("The access token expires in " + data.body["expires_in"]);
+      console.log("The access token is " + data.body["access_token"]);
 
-  //response.send(`You want to search for ${artistName}`);
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      spotifyApi
+        .searchArtists(artistName)
+        .then(data => response.send(data))
+        .catch(err => console.error(err));
+    },
+    function(err) {
+      console.error(
+        "Something went wrong when retrieving an access token",
+        err.message
+      );
+    }
+  );
 });
