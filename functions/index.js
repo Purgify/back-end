0f762d0b77550d1c4ca3731cc168631cfc852a96
Kelsey Huse <kelseyhuse30@gmail.com>
2019-09-21 10:39:53 -0500

    start firebase cloud function for get artist

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,9 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.detectArtist = functions.https.onRequest((request, response) => {
+  // get query param from request for the artist name to search
+  // call spotify API to get artist URI
+  // send success or error response to client
+});
