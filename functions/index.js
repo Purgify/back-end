46b8314fffec7a3603812913d996b2be842eabfa
Kelsey Huse <kelseyhuse30@gmail.com>
2019-09-21 11:02:19 -0500

    access spotify client id and secret

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const SPOTIFY_CLIENT_ID = functions.config().spotify.client_id;
+const SPOTIFY_CLIENT_SECRET = functions.config().spotify.client_secret;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,6 +13,7 @@ exports.detectArtist = functions.https.onRequest((request, response) => {
   // get query param from request for the artist name to search
   const artistName = request.query.name;
   // call spotify API to get artist URI
+
   // send success or error response to client
   response.send(`You want to search for ${artistName}`);
 });
